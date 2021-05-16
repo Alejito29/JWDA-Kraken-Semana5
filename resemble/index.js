13f3e846bbabb41ab158ac2a1b679828c649dee6
Angelica <am.rodriguez12@uniandes.edu.co>
2021-05-15 22:51:23 -0500

    Se agregan report4es faltantes

--- resemble/index.js
+++ resemble/index.js
@@ -9,8 +9,8 @@ async function executeTest(){
     let resultInfo = {}
 
     let path = `C:/Users/USUARIO/Desktop/VRTResemble/JWDA-Kraken-Semana5`  
-    let scenario = "Tag_manage_delete_tag"
-    let steps = 13
+    let scenario = "Login_invalid_user_valid_pass"
+    let steps = 6
 
 
     for(let i = 1; i < steps+1; i++){
